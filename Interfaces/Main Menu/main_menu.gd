9d3efc064d1a052a8caddd5c817a65675c36028b
pquangw <quangnp2005@gmail.com>
2026-07-20 15:04:51 +0700

    add quit function

--- Interfaces/Main Menu/main_menu.gd
+++ Interfaces/Main Menu/main_menu.gd
@@ -9,6 +9,11 @@ func _ready():
 func _on_start_button_pressed() -> void:
 	GameServer.start_client()
 
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+
 func _connect_button_sounds(button: Button) -> void:
 	button.mouse_entered.connect(func(): AudioManager.play_ui(AudioManager.sfx_button_hover))
 	button.pressed.connect(func(): AudioManager.play_ui(AudioManager.sfx_button_click))
