9fb697bb94eb5226aa5bd5269190fb1045d6291d
pquangw <quangnp2005@gmail.com>
2026-08-03 14:54:52 +0700

    fix warnings

--- Entities/Tank/texture_progress_bar.gd
+++ Entities/Tank/texture_progress_bar.gd
@@ -7,15 +7,15 @@ func _ready() -> void:
 	texture_under = create_circle_texture(8, Color(0.2, 0.2, 0.2))
 	value = 100
 
-func create_circle_texture(size: int, color: Color) -> Texture2D:
-	var image = Image.create(size, size, false, Image.FORMAT_RGBA8)
+func create_circle_texture(tsize: int, color: Color) -> Texture2D:
+	var image = Image.create(tsize, tsize, false, Image.FORMAT_RGBA8)
 	image.fill(Color(0, 0, 0, 0)) # transparent
 
-	var center = Vector2(size / 2, size / 2)
-	var radius = size / 2
+	var center = Vector2(tsize / 2.0, tsize / 2.0)
+	var radius = tsize / 2.0
 
-	for x in size:
-		for y in size:
+	for x in tsize:
+		for y in tsize:
 			var pos = Vector2(x, y)
 			if pos.distance_to(center) <= radius:
 				image.set_pixel(x, y, color)
